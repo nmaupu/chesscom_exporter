a488d495e5b7a237fa0e600cabb17ca24a05506f
Nicolas Maupu <nmaupu@gmail.com>
2021-10-07 00:48:41 +0200

    select all/none

--- pkg/ui/widget/archivesList.go
+++ pkg/ui/widget/archivesList.go
@@ -2,6 +2,8 @@ package widget
 
 import (
 	"gioui.org/layout"
+	"gioui.org/text"
+	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/nmaupu/chesscom_exporter/pkg/model"
@@ -14,6 +16,9 @@ type ArchiveList struct {
 	theme *material.Theme
 	list  widget.List
 	mutex sync.Mutex
+
+	selectAll  widget.Clickable
+	selectNone widget.Clickable
 }
 
 func NewArchiveList(th *material.Theme) *ArchiveList {
@@ -66,15 +71,65 @@ func (a *ArchiveList) Size() int {
 func (a *ArchiveList) Layout(gtx layout.Context) layout.Dimensions {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
-	return material.List(a.theme, &a.list).Layout(
-		gtx,
-		len(a.rows),
-		func(gtx layout.Context, i int) layout.Dimensions {
-			row := a.rows[i]
-			if row == nil {
-				return layout.Dimensions{}
-			}
-			return row.Layout(gtx)
-		},
+
+	selectAllClicked := a.selectAll.Clicked()
+	selectNoneClicked := a.selectNone.Clicked()
+	if selectAllClicked || selectNoneClicked {
+		for _, arch := range a.rows {
+			arch.checkbox.Value = selectAllClicked
+		}
+	}
+
+	return layout.Flex{
+		Spacing:   layout.SpaceEnd,
+		Axis:      layout.Vertical,
+		Alignment: layout.Start,
+	}.Layout(gtx,
+		layout.Rigid(a.layoutHeader),
+		layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+			return material.List(a.theme, &a.list).Layout(
+				gtx,
+				len(a.rows),
+				func(gtx layout.Context, i int) layout.Dimensions {
+					row := a.rows[i]
+					if row == nil {
+						return layout.Dimensions{}
+					}
+					return row.Layout(gtx)
+				},
+			)
+		}),
+	)
+}
+
+func (a *ArchiveList) layoutHeader(gtx layout.Context) layout.Dimensions {
+	return layout.Flex{
+		Axis:      layout.Horizontal,
+		Spacing:   layout.SpaceStart,
+		Alignment: layout.Middle,
+	}.Layout(gtx,
+		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+			return material.Clickable(gtx, &a.selectAll, func(gtx layout.Context) layout.Dimensions {
+				lbl := material.Label(a.theme, unit.Dp(16), "All")
+				lbl.Font.Style = text.Italic
+				if a.selectAll.Hovered() {
+					lbl.Font.Weight = text.DemiBold
+				}
+				return lbl.Layout(gtx)
+			})
+		}),
+		layout.Rigid(layout.Spacer{Width: unit.Dp(10)}.Layout),
+		layout.Rigid(material.Label(a.theme, unit.Dp(16), "|").Layout),
+		layout.Rigid(layout.Spacer{Width: unit.Dp(10)}.Layout),
+		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+			return material.Clickable(gtx, &a.selectNone, func(gtx layout.Context) layout.Dimensions {
+				lbl := material.Label(a.theme, unit.Dp(16), "None")
+				lbl.Font.Style = text.Italic
+				if a.selectNone.Hovered() {
+					lbl.Font.Weight = text.DemiBold
+				}
+				return lbl.Layout(gtx)
+			})
+		}),
 	)
 }
